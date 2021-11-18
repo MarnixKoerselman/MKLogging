bf42ffc632d28829b08bf2c6034a68a09a054720
Marnix <43467188+MarnixKoerselman@users.noreply.github.com>
2021-11-18 14:35:27 +0100

    Make it possible to redefine macros LOG_ENTER_LEAVE.

--- MKLoggingFiles/LogEnterLeave.h
+++ MKLoggingFiles/LogEnterLeave.h
@@ -3,8 +3,13 @@
 #include "ILogSink.h"
 #include "Logger.h"
 
+#ifndef MKL_LOG_ENTER_LEAVE
 #define MKL_LOG_ENTER_LEAVE(mp_Logger, mp_LogLevel) LogEnterLeave mgen_LogEnterLeave(mp_Logger, mp_LogLevel, __FUNCTION__, __FILE__, __LINE__);
+#endif // !MKL_LOG_ENTER_LEAVE
+
+#ifndef LOG_ENTER_LEAVE
 #define LOG_ENTER_LEAVE(mp_LogLevel) MKL_LOG_ENTER_LEAVE(LogCentral(), mp_LogLevel)
+#endif // !LOG_ENTER_LEAVE
 
 class LogEnterLeave
 {
