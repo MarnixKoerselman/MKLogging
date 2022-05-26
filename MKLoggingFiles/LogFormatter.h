080a92e4c2e2690acf205b206b6565ece865ae79
Marnix <MarnixKoerselman@users.noreply.github.com>
2022-05-26 11:47:21 +0200

    Process ID is not portable. Needs to be implemented on the target platform. For now, just remove the implementation. Pupose: separate portable code from Windows specific code.

--- MKLoggingFiles/LogFormatter.h
+++ MKLoggingFiles/LogFormatter.h
@@ -15,5 +15,4 @@ public:
   virtual void OutputLogLevel(std::ostream& os, ELogLevel logLevel);
   virtual void OutputTime(std::ostream& os, const std::chrono::system_clock::time_point& time);
   virtual void OutputThreadId(std::ostream& os, std::thread::id threadId);
-  virtual void OutputProcessId(std::ostream& os, uint32_t processId);
 };
