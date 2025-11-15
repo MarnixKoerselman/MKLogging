ec16a04207e29fdb1dbe3ee133f6de288f7b7040
Marnix <MarnixKoerselman@users.noreply.github.com>
2025-11-15 20:53:39 +0100

    version

--- generate-source-dist.bat
+++ generate-source-dist.bat
@@ -1,1 +1,1 @@
-cmake --preset windows-x64-vs2022 && cpack --config build/windows-x64-vs2022/CPackSourceConfig.cmake
+cmake --preset windows-x64-vs2022 -DPROJECT_VERSION_PARAM=%1 && cpack --config build/windows-x64-vs2022/CPackSourceConfig.cmake
