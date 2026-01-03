bc46e339c537ba3108cf37b37e9fa2de9a071edf
Marnix Koerselman <MarnixKoerselman@users.noreply.github.com>
2026-01-03 12:12:56 +0100

    Improve cpack source dist

--- generate-source-dist.bat
+++ generate-source-dist.bat
@@ -1,1 +1,1 @@
-cmake --preset win-x86-debug -DPROJECT_VERSION_PARAM=%1 && cpack --config build/win-x86-debug/CPackSourceConfig.cmake
+cmake --preset win-x86-debug -DSOURCE_DIST_PROJECT_VERSION=%1 && cpack --config build/win-x86-debug/CPackSourceConfig.cmake
