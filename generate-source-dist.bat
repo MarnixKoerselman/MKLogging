43c6192908bba7b47713e22cc24963b92e10b6c6
MarnixKoerselman <43467188+MarnixKoerselman@users.noreply.github.com>
2026-03-08 18:17:05 +0100

    fix source distribution, and test in gitlab actions with version 0.0.0

--- generate-source-dist.bat
+++ generate-source-dist.bat
@@ -1,1 +1,1 @@
-cmake --preset win-x86-debug -DSOURCE_DIST_PROJECT_VERSION=%1 && cpack --config build/win-x86-debug/CPackSourceConfig.cmake
+cmake --preset ninja-msvc-x86-debug -DSOURCE_DIST_PROJECT_VERSION=%1 && cpack --config build/ninja-msvc-x86-debug/CPackSourceConfig.cmake
